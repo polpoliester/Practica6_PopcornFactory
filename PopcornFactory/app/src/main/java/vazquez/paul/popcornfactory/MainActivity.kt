98e8a0889376bc96cccd02853b316bf50ceebf07
polpoliester <pol_poliester@hotmail.com>
2025-06-16 13:28:40 -0700

    MovieDetail terminado

--- PopcornFactory/app/src/main/java/vazquez/paul/popcornfactory/MainActivity.kt
+++ PopcornFactory/app/src/main/java/vazquez/paul/popcornfactory/MainActivity.kt
@@ -1,20 +1,18 @@
 package vazquez.paul.popcornfactory
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        var button: Button = findViewById<Button>(R.id.boton)
+        button.setOnClickListener {
+            var intento = Intent(this, CatalogActivity::class.java)
+            this.startActivity(intento)
         }
     }
 }
--- PopcornFactory/app/src/main/java/vazquez/paul/popcornfactory/MovieDetail.kt
+++ PopcornFactory/app/src/main/java/vazquez/paul/popcornfactory/MovieDetail.kt
@@ -1,20 +1,23 @@
 package vazquez.paul.popcornfactory
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.method.ScrollingMovementMethod
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MovieDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_movie_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val detalle = intent.extras
+
+        if (detalle != null) {
+            findViewById<TextView>(R.id.movie_title_detail).text = detalle.getString("titulo")
+            findViewById<TextView>(R.id.movie_desc).text = detalle.getString("sinopsis")
+            findViewById<TextView>(R.id.movie_desc).movementMethod = ScrollingMovementMethod()
+            findViewById<ImageView>(R.id.image_movie_detail).setImageResource(detalle.getInt("header"))
         }
     }
 }
